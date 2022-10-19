96204a4057b6bd599d0f5ad1f2e204b4aa7b1d84
vrfEducation <rahmanifard@live.com>
2022-10-19 14:07:29 +0330

    Listings and Realtors app updated

--- btre/realtors/models.py
+++ btre/realtors/models.py
@@ -1,3 +1,14 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length = 200)
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    description = models.TextField(blank= True)
+    phone = models.CharField(max_length = 20)
+    email = models.EmailField(max_length = 70)
+    is_mvp = models.BooleanField(default = False)
+    hire_date = models.DateTimeField(auto_now_add = True, blank = True)
+    
+    def __str__(self):
+        return self.name
